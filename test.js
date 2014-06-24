d9429e4b328e8749c1c822b342ba12b6f2db0f5c
Tyler Band <tylrm007@gmail.com>
2014-06-24 19:03:44 -0400

    rename security provider, debug and code cleanup

--- test.js
+++ test.js
@@ -4,9 +4,8 @@
 out = java.lang.System.out;
 
 var SysUtility = {
-
-	restPost : function restPost(url, params, settings, data) {
-		var restCaller = new com.kahuna.logic.lib.rest.RestCaller(this);
+	 restPost : function restPost(url, params, settings, data) {
+	    var restCaller = new com.kahuna.logic.lib.rest.RestCaller(this);
 		var result = restCaller.post(url, params, settings, data);
 		return result;
 	},
@@ -15,47 +14,47 @@ var SysUtility = {
 		var restCaller = new com.kahuna.logic.lib.rest.RestCaller(this);
 		var result = restCaller.get(url, params, settings);
 		return result;
-	}
+    }
 };
 
 // 1. load the class
-load("RESTSQLSecurityProvider.js");
+load("RESTAuthSecurityProvider.js");
 
 // 2. configuration needed for testing
 // change employee to the name of your view User_Login
 //if you have a different view for a list of all roles - you can chage the loginGroupURL to reflect that view
 //this information is stored on the Server and called when authentication is used.
 var configSetup = {
-   logonURL: 'https://demotest.espressologic.com/rest/el-test/demo/v1/employee',
+   loginBaseURL: 'https://demotest.espressologic.com/rest/el-test/demo/v1/employee',
    loginGroupURL : 'https://demotest.espressologic.com/rest/el-test/demo/v1/employee',
    logonApiKey: 'n3OKKu9OjxWP3HFYJQBm',
    keyLifetimeMinutes : 60
 };
 
 // 3.this is how the server creates the security object
-var restSqlClient = RESTAuthSecurityProviderCreate();
-restSqlClient.configure(configSetup);
+var restClient = RESTAuthSecurityProviderCreate();
+restClient.configure(configSetup);
 
-out.println("------------- testing sql authenticate with good payload----------------");
+out.println("------------- testing REST authenticate with good payload----------------");
 
 var payload = {
     username: "sam",
     password: "password1",
 };
 
-var result = restSqlClient.authenticate(payload);
+var result = restClient.authenticate(payload);
 out.println(JSON.stringify(result, null, 2));
 out.println("-------------");
 
 
-out.println("------------- testing sql authenticate with bad payload --------------");
+out.println("------------- testing REST authenticate with bad payload --------------");
 badPayload = {
     username: "badUserName",
     password: "Password$1",
 
 };
 
-result = restSqlClient.authenticate(badPayload);
+result = restClient.authenticate(badPayload);
 out.println(JSON.stringify(result, null, 2));
 out.println("-------------");
 
@@ -68,19 +67,21 @@ var payload = {
 
 
 out.println("------------- testing getAllGroups------------------");
-result = restSqlClient.getAllGroups();
+result = restClient.getAllGroups();
 out.println(JSON.stringify(result, null, 2));
 out.println("-------------");
 
 out.println("------------- testing getLoginInfo----------------");
 
-result = restSqlClient.getLoginInfo(null);
+//used only by LiveBrowser to build dynamic dialog box.
+result = restClient.getLoginInfo(null);
 out.println(JSON.stringify(result, null, 2));
 out.println("First field is " + result.fields[0].name);
 out.println("-------------");
 
+//used by the setup dialog
 out.println("------------- testing getConfigInfo---------------");
-result = restSqlClient.getConfigInfo();
+result = restClient.getConfigInfo();
 out.println(JSON.stringify(result, null, 2));
 out.println("First config prop is " + result.fields[0].name);
 out.println("-------------");
